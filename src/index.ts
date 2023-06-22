30149ec992ad0898db58953ca3f55e2ef43b446e
Sergey <69838796+Ooohstream@users.noreply.github.com>
2023-06-23 01:15:37 +0300

    migrated to Grammy, currentInfo renamed to info, calculate command

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,41 @@
 import 'dotenv/config';
-import { Telegraf } from 'telegraf';
 import { CalculationService } from './services';
 import { DURATION } from './constants';
+import { Bot, webhookCallback } from 'grammy';
+import express from 'express';
 
 const token = process.env.TOKEN;
 
 if (token) {
-  const bot = new Telegraf(token);
+  const bot = new Bot(token);
   let chats: Array<{ id: string | number; interval: NodeJS.Timer }> = [];
 
   bot.command('watch', async (ctx) => {
     if (chats.find(({ id }) => ctx.chat.id === id))
-      return await ctx.sendMessage('The watcher is already active!');
-    ctx.sendMessage(await CalculationService.getAverageMessage());
+      return await ctx.reply('The watcher is already active!');
+    await ctx.reply(await CalculationService.getAverageMessage());
     const interval = setInterval(async () => {
-      ctx.sendMessage(await CalculationService.getAverageMessage());
+      await ctx.reply(await CalculationService.getAverageMessage());
     }, DURATION);
     chats.push({ id: ctx.chat.id, interval });
     console.log(`The watcher has been started by ${ctx.chat.id}`);
   });
 
-  bot.command('currentInfo', async (ctx) => {
-    ctx.sendMessage(await CalculationService.getAverageMessage());
+  bot.command('info', async (ctx) => {
+    await ctx.reply(await CalculationService.getAverageMessage());
+  });
+
+  bot.command('calculate', async (ctx) => {
+    const message = String(ctx?.message?.text);
+    const coefficient = Number(message.split(' ')[1]);
+    const { rubTryAverageWithCommission } = await CalculationService.getAverageObject();
+    const calculatedRub = (rubTryAverageWithCommission * coefficient).toFixed(2);
+    await ctx.reply(`${calculatedRub}₽`);
   });
 
   bot.command('unwatch', async (ctx) => {
     if (!chats.find(({ id }) => ctx.chat.id === id))
-      return await ctx.sendMessage('The watcher is not active yet!');
+      return await ctx.reply('The watcher is not active yet!');
     chats = chats.filter(({ id, interval }) => {
       if (id === ctx.chat.id) {
         clearInterval(interval);
@@ -38,20 +47,22 @@ if (token) {
   });
 
   if (process.env.NODE_ENV === 'production') {
-    process.env.DOMAIN &&
-      process.env.PORT &&
-      bot.launch({
-        webhook: { domain: process.env.DOMAIN, port: parseInt(process.env.PORT) },
-      });
-    console.log('The bot has been started via webhook!');
+    const app = express();
+    const secretPath = String(process.env.TOKEN);
+    const domain = String(process.env.DOMAIN);
+    app.use(express.json());
+    app.use(`/${secretPath}`, webhookCallback(bot));
+    app.get('/', (req, res) => {
+      res.send({ message: 'ok' }).status(200);
+    });
+    app.listen(Number(process.env.PORT), async () => {
+      await bot.api.setWebhook(`https://${domain}/${secretPath}`);
+      console.log('The bot has been started via webhook!');
+    });
   } else {
-    bot.launch();
+    bot.start();
     console.log('The bot has been started via long-polling!');
   }
-
-  // Enable graceful stop
-  process.once('SIGINT', () => bot.stop('SIGINT'));
-  process.once('SIGTERM', () => bot.stop('SIGTERM'));
 } else {
   console.log(`Couldn't start, no token provided!`);
 }
--- src/services/calculation-service.ts
+++ src/services/calculation-service.ts
@@ -1,13 +1,24 @@
 import { Huobi } from '../api';
 
 export class CalculationService {
-  static getAverageMessage = async (): Promise<string> => {
+  static getAverageObject = async () => {
     const usdtAverage = parseFloat(await Huobi.getUsdtInfo());
     const tryAverage = parseFloat(await Huobi.getTryInfo());
     const rubTryAverage = usdtAverage / tryAverage;
     const rubTryAverageWithCommission = rubTryAverage + 0.05 * rubTryAverage;
-    return `USDT: ${usdtAverage} \nTRY: ${tryAverage} \nRUB -> TRY: ${rubTryAverageWithCommission.toFixed(
+
+    return {
+      usdtAverage,
+      tryAverage,
+      rubTryAverage,
+      rubTryAverageWithCommission,
+    };
+  };
+  static getAverageMessage = async (): Promise<string> => {
+    const { usdtAverage, tryAverage, rubTryAverageWithCommission } =
+      await CalculationService.getAverageObject();
+    return `USDT: ${usdtAverage}₽ \nTRY: ${tryAverage}₽ \nRUB -> TRY: ${rubTryAverageWithCommission.toFixed(
       2,
-    )}`;
+    )}₽`;
   };
 }
